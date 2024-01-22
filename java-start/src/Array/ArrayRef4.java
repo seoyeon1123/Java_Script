855cdb870551a62f9d4f90da0f9274814f96d48e
seoyeon1123 <woghks072@gmail.com>
2024-01-22 13:22:03 +0900

    feat : HODU-project

---
+++ java-start/src/Array/ArrayRef4.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class ArrayRef4 {
+    public static void main(String[] args) {
+        int[] students = {90, 80, 70, 60, 50, 20, 40, 10}; //배열 생성과 초기화 , 인트형 배열을 선언해주는구나 하고 알아줌.
+
+
+        //변수 값 사용
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("학생 " + (i + 1) + " 점수: " + students[i]);
+
+        }
+    }
+
+}
